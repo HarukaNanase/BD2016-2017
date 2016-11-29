ae37d7c83f473a52af82bd6cd5670ab246dae0dc
HarukaNanase <andrecaetanosoares@gmail.com>
2016-11-29 12:01:57 +0000

    Paga.data -> Paga.tempo

--- schema.sql
+++ schema.sql
@@ -138,7 +138,7 @@ FOREIGN KEY (numero) REFERENCES Reserva(numero) ON DELETE CASCADE
 CREATE TABLE Paga
 (
 numero int NOT NULL,
-data datetime NOT NULL,
+tempo datetime NOT NULL,
 metodo varchar(255) NOT NULL,
 
 PRIMARY KEY (numero),

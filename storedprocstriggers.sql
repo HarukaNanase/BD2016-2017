f4a9755263d47a8feb67a545e37624675554b811
HarukaNanase <andrecaetanosoares@gmail.com>
2016-11-30 16:34:27 +0000

    readded everything

---
+++ storedprocstriggers.sql
@@ -0,0 +1,51 @@
+
+	DROP TRIGGER if exists lastState;
+	delimiter //
+	CREATE TRIGGER lastState before insert on Paga FOR EACH ROW
+	BEGIN
+	declare ultimo datetime;
+
+SELECT max(`timestamp`) from Estado where numero = new.numero into ultimo;
+/*insert into Teste values (ultimo, new.`data`, timestampdiff(second,ultimo,new.`data`));
+*/
+IF timestampdiff(second,ultimo,new.`data`) <= 0
+THEN
+CALL raise_error;
+END IF;
+
+
+
+END; //
+
+
+
+DROP TRIGGER IF EXISTS overlappedDates;
+delimiter //
+
+CREATE TRIGGER overlappedDates before insert on Oferta FOR EACH ROW
+BEGIN
+declare exist int default 0;
+
+SELECT Count(B.codigo)
+FROM ((
+SELECT codigo,morada, data_inicio, data_fim
+FROM Oferta as O
+WHERE( O.codigo = new.codigo and 
+O.morada = new.morada and
+new.data_fim > new.data_inicio and (
+new.data_fim between O.data_inicio AND O.data_fim OR
+new.data_inicio between O.data_inicio and O.data_fim)
+OR (O.data_fim between new.data_inicio AND new.data_fim OR O.data_inicio between new.data_inicio and new.data_fim)))
+AS B )
+into exist;
+
+IF exist > 0 THEN
+CALL raise_error;
+END if;
+
+
+END; //
+
+delimiter ;
+
+

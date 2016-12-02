b5b8c19df751010a6efa6acb2ef08d81ddb93684
JoaoPedroCarvalho <joao.pedro.santos.carvalho@ist.utl.pt>
2016-12-02 20:57:22 +0000

    Fixed procurar

--- web/old/procurar_p2.php
+++ web/old/procurar_p2.php
@@ -7,7 +7,7 @@
 		$type = $_REQUEST['type'];
 		$morada = $_REQUEST['morada'];
 		require 'bd_init.php';
-        $sql = "SELECT oferta.codigo codigo, SUM(oferta.tarifa) total FROM edificio, alugavel, oferta WHERE edificio.morada = alugavel.morada AND alugavel.codigo = oferta.codigo AND alugavel.morada = oferta.morada AND edificio.morada = '$morada' GROUP BY edificio.morada , oferta.codigo;";    
+        $sql = "SELECT A.morada morada, A.codigo_espaco codigo, SUM(tarifa) total FROM oferta LEFT JOIN (SELECT * FROM posto UNION ALL (SELECT espaco.morada morada, espaco.codigo codigo, espaco.codigo codigo_espaco FROM espaco)) AS A ON oferta.morada = A.morada AND oferta.codigo = A.codigo Where A.morada = '$morada' GROUP BY oferta.morada , A.codigo_espaco;";    
         $result = $db->query($sql); 
 		echo("<table border=\"1\" cellspacing=\"2\">\n");
 		echo("<tr><td>codigo</td><td>Total Ganho</td></tr>\n");
@@ -20,7 +20,7 @@
         }
         echo("</table>\n");    
         $db = null;
-				echo("<td><a href=\"home.php\">HOME</a></td>\n");
+				echo("<td>	<a href=\"home.php\">HOME</a></td>\n");
     }
     catch (PDOException $e)
     {
